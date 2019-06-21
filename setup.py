bd01df9057bf6f9ca0e9de6be07b031587aafd02
Brandon Amos <brandon.amos.cs@gmail.com>
2019-06-21 08:33:41 -0400

    setup.py: Use py_modules.

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import find_packages, setup
 setup(
     name='lml',
     version='0.0.1',
-    description="TODO",
+    description="The limited multi-label projection layer.",
     author='Brandon Amos',
     author_email='brandon.amos.cs@gmail.com',
     platforms=['any'],
-    license="Apache 2.0",
+    license="MIT",
     url='https://github.com/locuslab/lml',
-    packages=find_packages(),
-    install_requires=[
+    py_modules=['lml'],
+    nstall_requires=[
         'numpy>=1<2',
     ]
 )
